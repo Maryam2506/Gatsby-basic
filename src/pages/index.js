0586289e4734284c49ad99d94491f97c132654ba
Maryam2506 <58718438+Maryam2506@users.noreply.github.com>
2020-06-27 08:42:09 +0500

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from 'gatsby';
+import Header from '../components/header';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default function Home() {
+  return (
+    <div style={{color: `purple`}}>
+      <Link to="/contect/">Contact</Link>
+      <Header headerText="Hello World!" />
+      <Link to="/about/">About</Link>
+      <Header headerText="It's pretty cool" />
+      <p>Welcome to my page</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+  )
+}
